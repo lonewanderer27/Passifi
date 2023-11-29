db3ed645e6e61a0467eda7be12a17a9295c5cb95
CortisolKing <cabilijohnandrei@gmail.com>
2023-11-29 17:42:21 +0800

    Init Repo

---
+++ app/src/main/java/com/ja_cabili/passifi/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.ja_cabili.passifi;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialize views
+        Button signInButton = findViewById(R.id.signInButton);
+
+        // Set click listener for Sign In button
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform Sign In action here or navigate to the next screen
+            }
+        });
+
+        // Set click listener for "Sign Up" text
+        TextView signUpText = findViewById(R.id.signUpText);
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle Sign Up text click, navigate to Sign Up screen or perform relevant action
+            }
+        });
+    }
+}
